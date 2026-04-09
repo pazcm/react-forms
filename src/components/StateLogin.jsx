43f9a4cd23da4b83f43aeac1a3bedc573a348c8a
Marta Paz <pazmarta@gmail.com>
2026-04-09 11:55:10 +0200

    set Input component
    Adding reusable `Input` custom component

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Input from './Input.jsx'
 
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState('');
@@ -56,35 +57,28 @@ export default function Login() {
       <h2>Testing State Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onBlur={() => handleInputBlur('email')}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-            value={enteredValues.email}
-          />
+        <Input 
+          label="Email" 
+          id="email" 
+          type="email" 
+          name="email" 
+          onBlur={() => handleInputBlur('email')}
+          onChange={(event) => handleInputChange('email', event.target.value)}
+          value={enteredValues.email} />
+
           <div className="control-error">
             {emailIsInvalid && <p>Please enter a valid email address here</p>}
           </div>
-        </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-            value={enteredValues.password}
-          />
-        </div>
-      </div>
-
+        <Input 
+          label="Password" 
+          id="password" 
+          type="password" 
+          name="password"
+          onBlur={() => handleInputBlur('password')}
+          onChange={(event) => handleInputChange('password', event.target.value)}
+          value={enteredValues.password} />
+    </div>
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
